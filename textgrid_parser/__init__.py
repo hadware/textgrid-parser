8a78f7187cc9ca72a1de3c64a1506079f5569124
hadware <hadwarez@gmail.com>
2022-08-13 21:00:20 +0200

    Started debugging parser

--- textgrid_parser/__init__.py
+++ textgrid_parser/__init__.py
@@ -27,13 +27,13 @@ class ParsingError(Exception):
             if self.token is None:
                 return "Unexpected end of string."
             else:
-                return f"Parsing error at index {self.token.index}, " \
+                return f"Parsing error at index {self.token.index}, line {self.token.lineno}, " \
                        f"unexpected token \"{self.token.value}\" (type {self.token.type})."
         else:
             if self.token is None:
                 return f"{self.msg} at EOF"
             else:
-                return f"{self.msg}, at index {self.token.index}."
+                return f"{self.msg}, at index {self.token.index}, line {self.token.lineno}."
 
 
 @dataclass
@@ -66,11 +66,28 @@ class TextTier(Tier):
 
 class TextGridLexer(Lexer):
     tokens = {
-
+        "FLOAT_LITERAL",
+        "INT_LITERAL",
+        "STRING_LITERAL",
+        "SIZE",
+        "INTERVALS",
+        "POINTS",
+        "CLASS",
+        "INTERVAL_TIER",
+        "TEXT_TIER",
+        "IDENTIFIER",
+        "ITEM",
+        "TIERS_EXIST"
     }
-    literals = {"=", ":", "tiers?", "<exist>", "[", "]"}
+    literals = {"=", ":", "[", "]", "<", ">"}
 
-    @_(r"[0-9]+\.[0.9]+")
+    ignore = '[ \t]+'
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)
+
+    @_(r"[0-9]+\.[0-9]+")
     def FLOAT_LITERAL(self, t):
         t.value = float(t.value)
         return t
@@ -80,20 +97,25 @@ class TextGridLexer(Lexer):
         t.value = int(t.value)
         return t
 
-    @_(r"\".*\"")
+    @_(r'".*"')
     def STRING_LITERAL(self, t):
         t.value = t.value.strip("")
         return t
 
-    IDENTIFIER = r"[a-ZA-Z ]+"
+    INTERVAL_TIER = '"IntervalTier"'
+    TEXT_TIER = '"TextTier"'
+    TIERS_EXIST = r"tiers\?"
+    IDENTIFIER = r"[a-zA-Z ?]+"
     IDENTIFIER["size"] = "SIZE"
     IDENTIFIER["intervals"] = "INTERVALS"
     IDENTIFIER["points"] = "POINTS"
-    ignore = ' \t'
-    ignore_newline = r'\n+'
+    IDENTIFIER["class"] = "CLASS"
+    IDENTIFIER["item"] = "ITEM"
+    #ITEM = r"ITEM"
 
 
 class TextGridParser(Parser):
+    debugfile = 'parser.out'
     tokens = TextGridLexer.tokens
     lexer = TextGridLexer()
 
@@ -117,41 +139,50 @@ class TextGridParser(Parser):
 
         self.parse(self.lexer.tokenize(tg_text))  # noqa
 
-    @_("{tg_property} tiers")
+    @_("{ tg_property } tiers")
     def textgrid(self, p: YaccProduction):
         # TODO: consistency checks
         return p.tiers
 
-    @_('"item" "[]" { tier }', )
-    def tiers(self, p: YaccProduction):
-        pass
+    @_('ITEM "[" "]" ":" { tier }')
+    def tiers(self, p: YaccProduction) -> List[Tier]:
+        return p[4]
 
     @_('interval_tier',
        'text_tier')
-    @_(' item_header { tg_property } { interval }')
+    def tier(self, p: YaccProduction) -> Union[Tier]:
+        return p[0]
+
+    @_('item_header CLASS "=" INTERVAL_TIER { tg_property } { interval }')
     def interval_tier(self, p: YaccProduction) -> Tier:
         tier_properties = dict(p.tg_property)
         return IntervalTier(tier_properties["name"], p.interval)
 
     @_('item_header tg_property tg_property tg_property')
     def interval(self, p: YaccProduction) -> Interval:
-        pass
+        point_properties = dict([p[1], p[2], p[3]])
+        return Interval(point_properties["start"],
+                        point_properties["end"],
+                        point_properties["mark"])
 
-    @_(' item_header { tg_property } { point }')
+    @_('item_header CLASS "=" TEXT_TIER { tg_property } { point }')
     def text_tier(self, p: YaccProduction) -> Tier:
         tier_properties = dict(p.tg_property)
         return TextTier(tier_properties["name"], p.interval)
 
     @_('item_header tg_property tg_property')
-    def point(self) -> Interval:
-        pass
+    def point(self, p: YaccProduction) -> Point:
+        point_properties = dict([p[1], p[2]])
+        return Point(point_properties["number"],
+                     point_properties["mark"])
 
     @_('IDENTIFIER "=" STRING_LITERAL',
+       'IDENTIFIER "=" INT_LITERAL',
        'IDENTIFIER "=" FLOAT_LITERAL')
     def tg_property(self, p: YaccProduction):
         return p[0], p[2]
 
-    @_('"tiers?" "<exist>"')
+    @_('TIERS_EXIST "<" IDENTIFIER ">"')
     def tg_property(self, p: YaccProduction) -> Tuple[str, bool]:
         return "has_tiers", True
 
@@ -160,6 +191,11 @@ class TextGridParser(Parser):
     def tg_property(self, p: YaccProduction):
         return p[2], p[4]
 
-    @_('IDENTIFIER "[" INT_LITERAL "]" ":"')
+    @_('ITEM "[" INT_LITERAL "]" ":"',
+       'INTERVALS "[" INT_LITERAL "]" ":"',
+       'POINTS "[" INT_LITERAL "]" ":"',)
     def item_header(self, p: YaccProduction) -> Tuple[str, int]:
-        return p.IDENTIFIER, p.INT_LITERAL
+        return p[0], p.INT_LITERAL
+
+    def error(self, token):
+        raise ParsingError(token)
